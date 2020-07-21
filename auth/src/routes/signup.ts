2dd2b6600a16f2ec33b0e6ddc0cf56f306a0021c
Xavier Rocamora <janzerker@hotmail.com>
2020-07-21 22:08:50 +0200

    normalization of error responses

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -16,14 +18,16 @@ router.post(
     // check if there's any validation errors
     const errors = validationResult(req);
 
-    // in which case send them as an array
+    // in which case throw a specific error an pass them as an array
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     console.log('Creating an user...');
+    // testing database error
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
