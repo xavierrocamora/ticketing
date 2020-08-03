0e1554a54b6dde6496baa15daa473c4206a28865
Xavier Rocamora <janzerker@hotmail.com>
2020-08-03 22:11:05 +0200

    implemented auth views/methods

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  // summon the getInitialProps of the Component trying to be rendered
+  // if the method is defined
+  // needed due to nextJS limitations only executing getInitialProps
+  // in the highest component
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
