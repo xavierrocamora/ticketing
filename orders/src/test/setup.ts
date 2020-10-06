bdf09f7d1b63ddba69f8a48329de6879771533f0
Xavier Rocamora <janzerker@hotmail.com>
2020-10-06 22:16:46 +0200

    implementing models, tests and routes for orders service

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,74 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+// tell jest to use a fake natsWrapper instance
+jest.mock('../nats-wrapper.ts');
+
+let mongo: any;
+// create an instance of mongo in memory,
+// and connect to it, before anything else starts
+beforeAll(async () => {
+  // harcoded environment key for testing purposes
+  process.env.JWT_KEY = 'asdfasdf';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// erase the db after each test
+// so it's ready for the next text
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// close connection with db instance
+// upon completion of all tests
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+// auxiliar function for handling authentication
+// while testing routes requiring it
+global.signin = () => {
+  // Building a JWT payload. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@testcom',
+  };
+
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string that's the cookie with the encoded data
+  // supertest handles cookies in an array so return an array
+  return [`express:sess=${base64}`];
+};
