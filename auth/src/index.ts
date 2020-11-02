3aa2fbdfc9fc7af3d8184f95d369c71a157a68ad
Xavier Rocamora <janzerker@hotmail.com>
2020-11-02 21:20:55 +0100

    fixed deployment images

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 //connect db and init server
 const start = async () => {
-  console.log('Starting up...');
+  console.log('Starting up....');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 
 //connect db and init server
 const start = async () => {
-  console.log('Starting...');
+  console.log('Starting....');
   
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
